8310b8d527244266d3b0544ddb2b92fee9dd42e5
DSSaumell <dsegurasmll@gmail.com>
2024-09-30 10:41:40 +0200

    PRUE

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo David Segura 24-25'),
+      home: const MyHomePage(title: 'Flutter Demo David Segura 24-25'), //configuració del títol
     );
   }
 }
